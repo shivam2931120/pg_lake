d781b0954354dd93787687f8220d4b9c8f413115
sfc-gh-npuka <naisila.puka@snowflake.com>
2026-04-20 11:55:20 +0200

    Move changes to pg_lake_iceberg--3.3--3.4.sql
    Signed-off-by: sfc-gh-npuka <naisila.puka@snowflake.com>

--- pg_lake_iceberg/pg_lake_iceberg--3.2--3.3.sql
+++ pg_lake_iceberg/pg_lake_iceberg--3.2--3.3.sql
@@ -14,29 +14,3 @@ CREATE OR REPLACE VIEW pg_catalog.iceberg_tables AS
 	SELECT catalog_name, table_namespace, table_name, metadata_location, previous_metadata_location
 	FROM lake_iceberg.tables
     WHERE metadata_location IS NOT NULL;
-
-/*
- * iceberg_catalog foreign data wrapper: allows defining named catalog
- * configurations via CREATE SERVER so that users are not limited to a
- * single global REST catalog configured through GUC settings.
- *
- * Example:
- *   CREATE SERVER my_polaris TYPE 'rest'
- *     FOREIGN DATA WRAPPER iceberg_catalog
- *     OPTIONS (rest_endpoint 'http://polaris:8181',
- *              rest_auth_type 'default',
- *              client_id '...',
- *              client_secret '...');
- *
- *   CREATE TABLE t (a int) USING iceberg WITH (catalog = 'my_polaris');
- */
-CREATE FUNCTION lake_iceberg.iceberg_catalog_validator(text[], oid)
-RETURNS void
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
-
-CREATE FOREIGN DATA WRAPPER iceberg_catalog
-  NO HANDLER
-  VALIDATOR lake_iceberg.iceberg_catalog_validator;
-
-GRANT USAGE ON FOREIGN DATA WRAPPER iceberg_catalog TO lake_write;
--- pg_lake_iceberg/pg_lake_iceberg--3.3--3.4.sql
+++ pg_lake_iceberg/pg_lake_iceberg--3.3--3.4.sql
@@ -1,1 +1,27 @@
 -- Upgrade script for pg_lake_iceberg from 3.3 to 3.4
+
+/*
+ * iceberg_catalog foreign data wrapper: allows defining named catalog
+ * configurations via CREATE SERVER so that users are not limited to a
+ * single global REST catalog configured through GUC settings.
+ *
+ * Example:
+ *   CREATE SERVER my_polaris TYPE 'rest'
+ *     FOREIGN DATA WRAPPER iceberg_catalog
+ *     OPTIONS (rest_endpoint 'http://polaris:8181',
+ *              rest_auth_type 'default',
+ *              client_id '...',
+ *              client_secret '...');
+ *
+ *   CREATE TABLE t (a int) USING iceberg WITH (catalog = 'my_polaris');
+ */
+CREATE FUNCTION lake_iceberg.iceberg_catalog_validator(text[], oid)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FOREIGN DATA WRAPPER iceberg_catalog
+  NO HANDLER
+  VALIDATOR lake_iceberg.iceberg_catalog_validator;
+
+GRANT USAGE ON FOREIGN DATA WRAPPER iceberg_catalog TO lake_write;
